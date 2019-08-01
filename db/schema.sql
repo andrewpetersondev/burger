181ae90ffd589af95527db3d23ad96f66447d62b
Andrew P <andrewpetersondev@gmail.com>
2019-08-01 02:37:29 -0500

    created database and burgers table

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,13 @@
 -- burger_name: a string.
 -- devoured: a boolean.
 
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR (60) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
