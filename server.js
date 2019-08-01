528a5b0dbae6b9f19e7f70f30e61c039d9202586
Andrew P <andrewpetersondev@gmail.com>
2019-08-01 08:35:16 -0500

    server js file is complete

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 // dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controllers");
+
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Create an instance of the express app.
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on port: " + PORT);
+});
