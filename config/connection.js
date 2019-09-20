5c5522ac821367f7ceedd339504d5a539305634a
Andrew P <andrewpetersondev@gmail.com>
2019-09-20 12:57:59 -0500

    connection file is complete

--- config/connection.js
+++ config/connection.js
@@ -4,26 +4,24 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "andrew",
-        password: "ThisIsMyPassword",
-        database: "burgers_db"
-    });
-};
-
-
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "andrew",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
 // make connection
-connection.connect(function (err) {
-    if (err) {
-        console.log("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // export connection for ORM to use
