f4526dce9781510161a6aba3974bc545b430234b
Andrew P <andrewpetersondev@gmail.com>
2019-09-20 14:41:06 -0500

    app responds to click events, need to fix model so burger devoured doesnt appear as null

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -8,11 +8,27 @@ $(function() {
         .trim()
     };
 
-    $.ajax("/api/burgers", {
+    $.ajax("/api/burgers/", {
       type: "POST",
       data: newBurger
     }).then(function() {
       console.log("adding a new burger");
+      location.reload();
+    });
+  });
+
+  // click a burger to eat it
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var devouredState = {
+      devoured: 1
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: devouredState
+    }).then(function() {
+      console.log("burger devoured");
       location.reload();
     });
   });
