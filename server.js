2ba761ce962126e15929e2b930dc1b345f875e4a
Andrew P <andrewpetersondev@gmail.com>
2019-09-21 12:23:21 -0500

    fully working app

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// routes
+// routesdgfadfad
 app.use(routes);
 
 // listeners
